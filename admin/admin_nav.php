5af2a1bc31a342b7a00c91279b58be68e6685bd0
Gas-nontachai <bigboy2546.77@gmail.com>
2024-09-10 21:26:44 +0700

    คู่มือแอดมิน

--- admin/admin_nav.php
+++ admin/admin_nav.php
@@ -122,7 +122,18 @@
                                 </div>
                             </a>
                         </li>
-
+                        <li class="nav-item">
+                            <a href="../asset/pdf/คู่มือการใช้งานระบบสำหรับผู้ดูแลระบบ.pdf" target="_blank" class="my-1 border nav-link link-dark btn btn-outline-info">
+                                <div class="mx-2 p-2 d-flex align-items-center ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="ms-3 bi bi-journal-bookmark" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M6 8V1h1v6.117L8.743 6.07a.5.5 0 0 1 .514 0L11 7.117V1h1v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8" />
+                                        <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2" />
+                                        <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z" />
+                                    </svg>
+                                    <strong class="mx-2 ">คู่มือการใช้งาน</strong>
+                                </div>
+                            </a>
+                        </li>
                     </ul>
                     <hr>
                 </div>
